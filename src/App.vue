db0c487495f03d6f2b6da2297f147ec24ba86f08
Yang <94534613+BoyYangzai@users.noreply.github.com>
2022-03-17 11:21:28 +0800

    fix:对lint_staged的一次测试

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue';
+let b = {
+    name: 'yang',
+
+    test: 'qaq',
+};
 </script>
 <style>
 #app {
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
+console.log('woaini');
+const p = {
+    name: 'Yang',
+};
