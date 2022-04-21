37780a6f2afaa5a307cf456fbe3d5f9a2e3916db
Yang <94534613+BoyYangzai@users.noreply.github.com>
2022-04-21 14:22:32 +0800

    chore: 添加图片压缩插件

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
     plugins: [
@@ -13,6 +14,33 @@ export default defineConfig({
         }),
         Components({
             resolvers: [ElementPlusResolver()]
+        }),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false
+            },
+            optipng: {
+                optimizationLevel: 7
+            },
+            mozjpeg: {
+                quality: 20
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox'
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false
+                    }
+                ]
+            }
         })
     ],
     resolve: {
