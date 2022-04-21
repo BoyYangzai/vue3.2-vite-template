741b993df23b1267d77d9dfb957bd5275434b3d8
Yang <94534613+BoyYangzai@users.noreply.github.com>
2022-04-21 14:23:55 +0800

    chore: 默认配置注释,选择性使用

--- src/main.ts
+++ src/main.ts
@@ -9,47 +9,41 @@ createApp(App)
     .use(createPinia())
     .mount('#app');
 
-//全局路由守卫
-const store = useStore();
-router.beforeEach((to, from, next) => {
-    // store.tagList = JSON.parse(window.localStorage.getItem("tagList") as string);
-    //pinia状态持久化
-    if (to.name !== 'login') {
-        if (
-            window.localStorage.getItem(
-                'token'
-            ) !== null &&
-            window.localStorage.getItem(
-                'token'
-            ) != 'null'
-        ) {
-            store.getUserInfo(
-                window.localStorage.getItem(
-                    'userId'
-                ) as string
-            );
-            next();
-        } else if (
-            window.localStorage.getItem(
-                'token'
-            ) == null ||
-            window.localStorage.getItem(
-                'token'
-            ) == 'null'
-        ) {
-            next({
-                name: 'login'
-            });
-        } else {
-            next();
-        }
-    } else {
-        next();
-    }
-});
-//test
-console.log('woaini');
-const p = {
-    name: 'Yang',
-    age: '18'
-};
+// //全局路由守卫
+// const store = useStore();
+// router.beforeEach((to, from, next) => {
+//     // store.tagList = JSON.parse(window.localStorage.getItem("tagList") as string);
+//     //pinia状态持久化
+//     if (to.name !== 'login') {
+//         if (
+//             window.localStorage.getItem(
+//                 'token'
+//             ) !== null &&
+//             window.localStorage.getItem(
+//                 'token'
+//             ) != 'null'
+//         ) {
+//             store.getUserInfo(
+//                 window.localStorage.getItem(
+//                     'userId'
+//                 ) as string
+//             );
+//             next();
+//         } else if (
+//             window.localStorage.getItem(
+//                 'token'
+//             ) == null ||
+//             window.localStorage.getItem(
+//                 'token'
+//             ) == 'null'
+//         ) {
+//             next({
+//                 name: 'login'
+//             });
+//         } else {
+//             next();
+//         }
+//     } else {
+//         next();
+//     }
+// });
--- src/network/login/login.ts
+++ src/network/login/login.ts
@@ -1,81 +1,81 @@
-import { ElMessage } from 'element-plus';
-import { request } from '../network';
-interface getBody {
-    studentId: string;
-}
-interface postBody {
-    studentId: string;
-    password: string;
-}
-export function getrequest(param: string) {
-    return request.get(param);
-}
-export function postrequest(
-    param: string,
-    body: postBody
-) {
-    return request.post(param, body);
-}
-request.interceptors.request.use(
-    (config: any) => {
-        if (
-            window.localStorage.getItem(
-                'token'
-            ) != 'null'
-        ) {
-            config!.headers!.Authorization = `Bearer ${window.localStorage.getItem(
-                'token'
-            )}`;
-        }
-        console.log(
-            config!.headers!.Authorization
-        );
+// import { ElMessage } from 'element-plus';
+// import { request } from '../network';
+// interface getBody {
+//     studentId: string;
+// }
+// interface postBody {
+//     studentId: string;
+//     password: string;
+// }
+// export function getrequest(param: string) {
+//     return request.get(param);
+// }
+// export function postrequest(
+//     param: string,
+//     body: postBody
+// ) {
+//     return request.post(param, body);
+// }
+// request.interceptors.request.use(
+//     (config: any) => {
+//         if (
+//             window.localStorage.getItem(
+//                 'token'
+//             ) != 'null'
+//         ) {
+//             config!.headers!.Authorization = `Bearer ${window.localStorage.getItem(
+//                 'token'
+//             )}`;
+//         }
+//         console.log(
+//             config!.headers!.Authorization
+//         );
 
-        return config;
-    }
-);
-request.interceptors.response.use(
-    (res: any) => {
-        if (res.status == 200) {
-            console.log(res);
-            if (res.data.code == 200) {
-                ElMessage.success({
-                    message: res.data.message,
-                    center: true
-                });
-                return res;
-            } else if (res.data.code == 1003) {
-                ElMessage.error({
-                    message: res.data.message,
-                    center: true
-                });
-                return;
-            } else if (res.data.code == 1002) {
-                ElMessage({
-                    message: res.data.message,
-                    center: true
-                });
-                return;
-            } else if (res.data.code == 1001) {
-                ElMessage({
-                    message: res.data.message,
-                    center: true
-                });
-                return;
-            }
-        } else {
-            console.log('error');
-            ElMessage.error({
-                message: res.data.message,
-                center: true
-            });
-        }
-    },
-    (err: any) => {
-        ElMessage.error({
-            message: '请检查网络设置',
-            center: true
-        });
-        return;
-    }
-);
+//         return config;
+//     }
+// );
+// request.interceptors.response.use(
+//     (res: any) => {
+//         if (res.status == 200) {
+//             console.log(res);
+//             if (res.data.code == 200) {
+//                 ElMessage.success({
+//                     message: res.data.message,
+//                     center: true
+//                 });
+//                 return res;
+//             } else if (res.data.code == 1003) {
+//                 ElMessage.error({
+//                     message: res.data.message,
+//                     center: true
+//                 });
+//                 return;
+//             } else if (res.data.code == 1002) {
+//                 ElMessage({
+//                     message: res.data.message,
+//                     center: true
+//                 });
+//                 return;
+//             } else if (res.data.code == 1001) {
+//                 ElMessage({
+//                     message: res.data.message,
+//                     center: true
+//                 });
+//                 return;
+//             }
+//         } else {
+//             console.log('error');
+//             ElMessage.error({
+//                 message: res.data.message,
+//                 center: true
+//             });
+//         }
+//     },
+//     (err: any) => {
+//         ElMessage.error({
+//             message: '请检查网络设置',
+//             center: true
+//         });
+//         return;
+//     }
+// );
